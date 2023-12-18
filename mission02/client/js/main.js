b77a903b8888b2ea03ddc730e7ba0f06eca0c9d4
hyun9758 <jj020305@likelion.org>
2023-12-18 11:21:30 +0900

    대상 잡기

---
+++ mission02/client/js/main.js
@@ -0,0 +1,20 @@
+/* 
+
+1. 클릭 이벤트 활성화
+2. nav 클릭시 배경 색상 변경
+3. 이미지 변경
+4. 텍스트 변경
+5. 함수 분리
+
+*/
+
+const nav = document.querySelector(".nav");
+
+function handleClick(e) {
+  const target = e.target;
+
+  const li = target.closest("li");
+
+  if (!li) return;
+}
+nav.addEventListener("click", handleClick);
